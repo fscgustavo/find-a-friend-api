0fd14a8eaee80b3266d8596c3178ad53538cae9e
Gustavo Fonseca <gus.fonnseca@gmail.com>
2023-11-14 18:52:08 -0300

    chore: inital setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# App
+
+### Application rules
+
+- [ ] It must be possible to register a pet
+- [ ] It should be possible to list all pets available for adoption in a city
+- [ ] It should be possible to filter pets by their characteristics
+- [ ] It must be possible to view details of a pet up for adoption
+- [ ] It must be possible to register as an ORG
+- [ ] It must be possible to log in as an ORG
+
+### Business rules
+
+- [ ] To list pets, we must inform the city
+- [ ] An ORG must have an address and a WhatsApp number
+- [ ] A pet must be linked to an ORG
+- [ ] Users who want to adopt will contact the ORG via WhatsApp
+- [ ] All filters, besides the city, are indirectly
+- [ ] For an ORG to access an application as admin, it must be logged in
---
+++ prisma/migrations/20231115143848_create_user_and_org/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "energy" INTEGER NOT NULL,
+    "size" TEXT NOT NULL,
+    "independence" TEXT NOT NULL,
+    "adoptionRequirements" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_email_key" ON "organizations"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
